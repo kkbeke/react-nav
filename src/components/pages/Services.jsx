6078df7deb4b81f5f81c66c8ec4517dd49053a57
Carlos Echavez <carlosechavezjr@gmail.com>
2023-08-31 22:16:57 +0800

    first commit

---
+++ src/components/pages/Services.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+
+export const Services = () => {
+  return (
+    <div>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla qui
+        alias aut? Odit quam autem sint quisquam, voluptas fuga, nihil explicabo
+        pariatur reprehenderit earum accusantium harum, est voluptates nulla
+        iusto id. Sed excepturi minus, doloremque animi asperiores cum harum
+        aliquid nihil eius voluptates perspiciatis neque ut nam consectetur
+        maiores. Quaerat mollitia doloremque animi natus nam est possimus, odit
+        aliquid assumenda. Delectus qui praesentium nemo aliquam officiis optio
+        quasi eligendi dolores! Exercitationem esse repudiandae vel atque
+        ducimus at libero molestias, hic voluptatibus aspernatur repellendus
+        dolor quibusdam impedit suscipit quisquam dolorum cumque ab nemo facilis
+        deleniti dolores consequatur obcaecati fugit tenetur? Labore!
+      </div>
+      <br />
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla qui
+        alias aut? Odit quam autem sint quisquam, voluptas fuga, nihil explicabo
+        pariatur reprehenderit earum accusantium harum, est voluptates nulla
+        iusto id. Sed excepturi minus, doloremque animi asperiores cum harum
+        aliquid nihil eius voluptates perspiciatis neque ut nam consectetur
+        maiores. Quaerat mollitia doloremque animi natus nam est possimus, odit
+        aliquid assumenda. Delectus qui praesentium nemo aliquam officiis optio
+        quasi eligendi dolores! Exercitationem esse repudiandae vel atque
+        ducimus at libero molestias, hic voluptatibus aspernatur repellendus
+        dolor quibusdam impedit suscipit quisquam dolorum cumque ab nemo facilis
+        deleniti dolores consequatur obcaecati fugit tenetur? Labore!
+      </div>
+      <br />
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla qui
+        alias aut? Odit quam autem sint quisquam, voluptas fuga, nihil explicabo
+        pariatur reprehenderit earum accusantium harum, est voluptates nulla
+        iusto id. Sed excepturi minus, doloremque animi asperiores cum harum
+        aliquid nihil eius voluptates perspiciatis neque ut nam consectetur
+        maiores. Quaerat mollitia doloremque animi natus nam est possimus, odit
+        aliquid assumenda. Delectus qui praesentium nemo aliquam officiis optio
+        quasi eligendi dolores! Exercitationem esse repudiandae vel atque
+        ducimus at libero molestias, hic voluptatibus aspernatur repellendus
+        dolor quibusdam impedit suscipit quisquam dolorum cumque ab nemo facilis
+        deleniti dolores consequatur obcaecati fugit tenetur? Labore!
+      </div>
+      <br />
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla qui
+        alias aut? Odit quam autem sint quisquam, voluptas fuga, nihil explicabo
+        pariatur reprehenderit earum accusantium harum, est voluptates nulla
+        iusto id. Sed excepturi minus, doloremque animi asperiores cum harum
+        aliquid nihil eius voluptates perspiciatis neque ut nam consectetur
+        maiores. Quaerat mollitia doloremque animi natus nam est possimus, odit
+        aliquid assumenda. Delectus qui praesentium nemo aliquam officiis optio
+        quasi eligendi dolores! Exercitationem esse repudiandae vel atque
+        ducimus at libero molestias, hic voluptatibus aspernatur repellendus
+        dolor quibusdam impedit suscipit quisquam dolorum cumque ab nemo facilis
+        deleniti dolores consequatur obcaecati fugit tenetur? Labore!
+      </div>
+      <br />
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla qui
+        alias aut? Odit quam autem sint quisquam, voluptas fuga, nihil explicabo
+        pariatur reprehenderit earum accusantium harum, est voluptates nulla
+        iusto id. Sed excepturi minus, doloremque animi asperiores cum harum
+        aliquid nihil eius voluptates perspiciatis neque ut nam consectetur
+        maiores. Quaerat mollitia doloremque animi natus nam est possimus, odit
+        aliquid assumenda. Delectus qui praesentium nemo aliquam officiis optio
+        quasi eligendi dolores! Exercitationem esse repudiandae vel atque
+        ducimus at libero molestias, hic voluptatibus aspernatur repellendus
+        dolor quibusdam impedit suscipit quisquam dolorum cumque ab nemo facilis
+        deleniti dolores consequatur obcaecati fugit tenetur? Labore!
+      </div>
+      <br />
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla qui
+        alias aut? Odit quam autem sint quisquam, voluptas fuga, nihil explicabo
+        pariatur reprehenderit earum accusantium harum, est voluptates nulla
+        iusto id. Sed excepturi minus, doloremque animi asperiores cum harum
+        aliquid nihil eius voluptates perspiciatis neque ut nam consectetur
+        maiores. Quaerat mollitia doloremque animi natus nam est possimus, odit
+        aliquid assumenda. Delectus qui praesentium nemo aliquam officiis optio
+        quasi eligendi dolores! Exercitationem esse repudiandae vel atque
+        ducimus at libero molestias, hic voluptatibus aspernatur repellendus
+        dolor quibusdam impedit suscipit quisquam dolorum cumque ab nemo facilis
+        deleniti dolores consequatur obcaecati fugit tenetur? Labore!
+      </div>
+      <br />
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla qui
+        alias aut? Odit quam autem sint quisquam, voluptas fuga, nihil explicabo
+        pariatur reprehenderit earum accusantium harum, est voluptates nulla
+        iusto id. Sed excepturi minus, doloremque animi asperiores cum harum
+        aliquid nihil eius voluptates perspiciatis neque ut nam consectetur
+        maiores. Quaerat mollitia doloremque animi natus nam est possimus, odit
+        aliquid assumenda. Delectus qui praesentium nemo aliquam officiis optio
+        quasi eligendi dolores! Exercitationem esse repudiandae vel atque
+        ducimus at libero molestias, hic voluptatibus aspernatur repellendus
+        dolor quibusdam impedit suscipit quisquam dolorum cumque ab nemo facilis
+        deleniti dolores consequatur obcaecati fugit tenetur? Labore!
+      </div>
+      <br />
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla qui
+        alias aut? Odit quam autem sint quisquam, voluptas fuga, nihil explicabo
+        pariatur reprehenderit earum accusantium harum, est voluptates nulla
+        iusto id. Sed excepturi minus, doloremque animi asperiores cum harum
+        aliquid nihil eius voluptates perspiciatis neque ut nam consectetur
+        maiores. Quaerat mollitia doloremque animi natus nam est possimus, odit
+        aliquid assumenda. Delectus qui praesentium nemo aliquam officiis optio
+        quasi eligendi dolores! Exercitationem esse repudiandae vel atque
+        ducimus at libero molestias, hic voluptatibus aspernatur repellendus
+        dolor quibusdam impedit suscipit quisquam dolorum cumque ab nemo facilis
+        deleniti dolores consequatur obcaecati fugit tenetur? Labore!
+      </div>
+      <br />
+    </div>
+  );
+};
